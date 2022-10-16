86c67dfff40de879c6d471c0c1bb11e1aa0e3ad6
Martin House <torchester@gmail.com>
2022-10-16 18:32:59 +0500

    add task 8

--- homework_001/Program.cs
+++ homework_001/Program.cs
@@ -65,3 +65,22 @@ else
 }
 */
 
+//Задача 8: Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+/*
+Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = 1;
+
+while (current <= num)
+{
+    if (current % 2 == 0)
+    {
+        Console.Write(current + " ");
+    }
+
+    current++;
+}
+*/
