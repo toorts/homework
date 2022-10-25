9ad9d9dbbcff9ae4373ae4d6185b925eac0bc1e4
Martin House <torchester@gmail.com>
2022-10-25 18:30:59 +0500

    add task 13

--- homework_002/Program.cs
+++ homework_002/Program.cs
@@ -13,3 +13,30 @@
 // int num = Convert.ToInt32(Console.ReadLine());
 
 // Console.WriteLine($"Second number of {num} is {showSecondNum(num)}");
+
+//* Задача 13: Напишите программу, которая выводит третью цифру заданного числа
+//* или сообщает, что третьей цифры нет.
+
+// int ShowThirdNum(int number)
+// {
+//     if (number > 1000)
+//     {
+//         while (number > 1000)
+//         {
+//             number = number / 10;
+//         }
+//     }
+
+//     if (number < 1000 && number > 99)
+//     {
+//         number = number % 10;
+//         return number;
+//     }
+
+//     else return -1;
+// }
+
+// Console.Write("Input number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine($"Third number of {num} is {ShowThirdNum(num)}"); 
