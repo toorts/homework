2b26e21fb0880b8fc1b8ab03317fc5ee1c5f3455
Martin House <torchester@gmail.com>
2022-10-25 18:00:14 +0500

    add task 10

---
+++ homework_002/Program.cs
@@ -0,0 +1,15 @@
+﻿//* Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+//* и на выходе показывает вторую цифру этого числа.
+
+// int showSecondNum(int number)
+// {
+//     int cutThird = number / 10;
+//     int result = cutThird % 10;
+
+//     return result;
+// }
+
+// Console.Write("Input 3-digit number: "); 
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine($"Second number of {num} is {showSecondNum(num)}");
