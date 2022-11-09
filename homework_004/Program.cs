19de9e5a23dec30dc683a0cf3b6b88ad00dd7a75
Martin House <torchester@gmail.com>
2022-11-09 20:29:12 +0500

    add task 25

---
+++ homework_004/Program.cs
@@ -0,0 +1,71 @@
+﻿//* Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+//* и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+// int NumPower(int number, int power)
+// {
+//     int result = 1;
+
+//     for (int current = 1; current <= power; current++)
+//     {
+//         result *= number;
+//     }
+
+//     return result;
+// }
+
+// Console.Write("Input a number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Input a power: ");
+// int pow = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine($"{num} to the power of {pow} equals {NumPower(num, pow)}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//* Задача 27: Напишите программу, которая принимает на вход число
+//* и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+//* Задача 29: Напишите программу, которая задаёт массив из m элементов
+//* и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
