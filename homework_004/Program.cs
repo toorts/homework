07fcd15251fbc522933b016e84020bd4b9ea9bea
Martin House <torchester@gmail.com>
2022-11-10 08:02:30 +0500

    add task 29

--- homework_004/Program.cs
+++ homework_004/Program.cs
@@ -57,3 +57,33 @@
 
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
+
+// int[] CreateArray(int size)
+// {
+//     int[] array = new int[size];
+
+//     for (int i = 0; i < size; i++)
+//     {   
+//         Console.Write($"Input number {i + 1}: ");
+//         array[i] = Convert.ToInt32(Console.ReadLine());
+//     }
+        
+//     return array;
+// }
+
+// void ShowArray(int[] array)
+// {
+//     Console.Write("{ ");
+
+//     for (int i = 0; i < array.Length; i++)
+//         Console.Write(array[i] + " ");
+
+//     Console.Write("}");
+// }
+
+// Console.Write("Input a number of elements: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int[] numList = CreateArray(num);
+
+// ShowArray(numList);
