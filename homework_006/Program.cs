55fcfc545ac3e59f0c581ca763b82e66b911a567
Martin House <torchester@gmail.com>
2022-11-15 20:29:13 +0500

    upd fix 43

--- homework_006/Program.cs
+++ homework_006/Program.cs
@@ -29,24 +29,24 @@
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-Console.Write("Input b1: ");
-double b1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Input k1: ");
-double k1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Input b2: ");
-double b2 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Input k2: ");
-double k2 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Input b1: ");
+// double b1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Input k1: ");
+// double k1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Input b2: ");
+// double b2 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Input k2: ");
+// double k2 = Convert.ToDouble(Console.ReadLine());
 
-if (b1 == b2 && k1 == k2)
-    Console.WriteLine("These are the same lines.");
+// if (b1 == b2 && k1 == k2)
+//     Console.WriteLine("These are the same lines.");
 
-else if (k1 == k2)
-    Console.WriteLine("These are parallel lines.");
+// else if (k1 == k2)
+//     Console.WriteLine("These are parallel lines.");
 
-else
-{
-    double x = Math.Round((b2 - b1) / (k1 - k2), 2);
-    double y = Math.Round(k2 * x + b2, 2);
-    Console.WriteLine($"Point of intersection is ({x}; {y})");
-}
+// else
+// {
+//     double x = Math.Round((b2 - b1) / (k1 - k2), 2);
+//     double y = Math.Round(k2 * x + b2, 2);
+//     Console.WriteLine($"Point of intersection is ({x}; {y})");
+// }
