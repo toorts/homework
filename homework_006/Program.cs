3da61a2145fc1fc5fb4446a5dc4b3c106bef6db8
Martin House <torchester@gmail.com>
2022-11-15 19:24:03 +0500

    add task 43

--- homework_006/Program.cs
+++ homework_006/Program.cs
@@ -25,6 +25,28 @@
 
 
 //* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
-//*  заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//* заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
-// b1 = 2, k1 = 5, b2 = 4,    k2 = 9 -> (-0,5; -0,5)
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// Console.Write("Input b1: ");
+// double b1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Input k1: ");
+// double k1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Input b2: ");
+// double b2 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Input k2: ");
+// double k2 = Convert.ToDouble(Console.ReadLine());
+
+// if (b1 == b2 && k1 == k2)
+//     Console.WriteLine("These are the same lines.");
+
+// else if (k1 == k2)
+//     Console.WriteLine("These are parallel lines.");
+
+// else
+// {
+//     double x = (b2 - b1) / (k1 - k2);
+//     double y = k2 * x + b2;
+//     Console.WriteLine($"Point of intersection is ({x}, {y})");
+// }
